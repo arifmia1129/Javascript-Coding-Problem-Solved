0d88f88200a727523b75338bfa822f6e8471cf2e
arifmia1129 <arifmia1129@gmail.com>
2023-11-23 11:37:36 +0600

    [solve] function for sort array base on for loop

--- index.js
+++ index.js
@@ -79,4 +79,25 @@ const isPrime = (num) => {
   return true;
 };
 
-console.log(isPrime(3));
+// console.log(isPrime(3));
+
+// sort array
+
+function sortArray(arr) {
+  const length = arr.length;
+
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+
+console.log(sortArray(unsortedArray));
